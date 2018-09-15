4c273a61b63535fc32786fcc9f79118071cbf16a
KennethAndrew <kives@deloitte.ca>
2018-09-15 00:14:27 -0400

    mistakes were made

--- Dockerfile
+++ Dockerfile
@@ -4,24 +4,8 @@ WORKDIR /
 
 COPY . .
 
-RUN yes|apt-get update
-RUN yes|apt-get install build-essential
-RUN yes|apt-get install sqlite3 libsqlite3-dev
-RUN yes|apt-get install libssl-dev
-RUN yes|apt-get install libevent-dev
-RUN yes|apt-get install -y wget 
-RUN rm -rf /var/lib/apt/lists/*
-
-RUN wget http://turnserver.open-sys.org/downloads/v4.5.0.5/turnserver-4.5.0.5.tar.gz
-RUN tar -xzvf turnserver-4.5.0.5.tar.gz
-RUN cd turnserver-4.5.0.5
-RUN ./configure	
-RUN make
-RUN make install
-RUN cd
-
 RUN chmod +x configure.bash
-RUN ./configure.bash
+&& ./configure.bash
 
 
 
--- configure.bash
+++ configure.bash
@@ -15,6 +15,22 @@
 #make
 #sudo make install
 #cd
+#
+ yes|apt-get update
+ yes|apt-get install build-essential
+ yes|apt-get install sqlite3 libsqlite3-dev
+ yes|apt-get install libssl-dev
+ yes|apt-get install libevent-dev
+ yes|apt-get install -y wget 
+ rm -rf /var/lib/apt/lists/*
+
+ wget http://turnserver.open-sys.org/downloads/v4.5.0.5/turnserver-4.5.0.5.tar.gz
+ tar -xzvf turnserver-4.5.0.5.tar.gz
+ cd turnserver-4.5.0.5
+ ./configure	
+ make
+ make install
+ cd
 
 publichostname=$(curl http://169.254.169.254/latest/meta-data/public-hostname 2>/dev/null)
 publicip=$(dig +short myip.opendns.com @resolver1.opendns.com)
