e48327584bb3fb527fd483caaade04d3a165383e
KennethAndrew <kives@deloitte.ca>
2018-09-14 23:37:44 -0400

    this one might actually work!

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,32 @@
 FROM ubuntu:16.04
 
-
-# Create app directory
 WORKDIR /
 
 COPY . .
+
+RUN sudo apt-get update
+RUN sudo apt-get install build-essential
+RUN sudo apt-get install sqlite3 libsqlite3-dev
+RUN sudo apt-get install libssl-dev
+
+RUN cd libevent
+RUN tar -xzvf libevent-2.0.21-stable.tar.gz
+RUN cd libevent-2.0.21-stable
+RUN ./configure
+RUN make
+RUN sudo make install
+RUN cd
+
+RUN cd coturn 
+RUN tar -xzvf coturn_4.5.0.7.orig.tar.gz
+RUN cd turnserver-4.5.0.7
+RUN ./configure
+RUN make
+RUN sudo make install
+RUN cd
+
 RUN chmod +x configure.bash
 RUN ./configure.bash
+
+
 
--- configure.bash
+++ configure.bash
@@ -1,25 +1,20 @@
 #!/usr/bin/env bash
 
-sudo apt-get update
-sudo apt-get install build-essential
-sudo apt-get install sqlite3 libsqlite3-dev
-sudo apt-get install libssl-dev
+#cd libevent
+#tar -xzvf libevent-2.0.21-stable.tar.gz
+#cd libevent-2.0.21-stable
+#./configure
+#make
+#sudo make install
+#cd
 
-cd libevent
-tar -xzvf libevent-2.0.21-stable.tar.gz
-cd libevent-2.0.21-stable
-./configure
-make
-sudo make install
-cd
-
-cd coturn 
-tar -xzvf coturn_4.5.0.7.orig.tar.gz
-cd turnserver-4.5.0.7
-./configure
-make
-sudo make install
-cd
+#cd coturn 
+#tar -xzvf coturn_4.5.0.7.orig.tar.gz
+#cd turnserver-4.5.0.7
+#./configure
+#make
+#sudo make install
+#cd
 
 publichostname=$(curl http://169.254.169.254/latest/meta-data/public-hostname 2>/dev/null)
 publicip=$(dig +short myip.opendns.com @resolver1.opendns.com)
