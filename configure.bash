f816274a188065a586dbded3b22701b66564e952
KennethAndrew <kives@deloitte.ca>
2018-09-15 01:02:13 -0400

    fuck

--- configure.bash
+++ configure.bash
@@ -33,13 +33,10 @@
  make install
  cd
 
+ 
 publichostname=$(curl http://169.254.169.254/latest/meta-data/public-hostname 2>/dev/null)
 publicip=$(dig +short myip.opendns.com @resolver1.opendns.com)
-privateip=$(hostname -I)
-echo privateip
-echo publichostname
-echo publicip
+#privateip=$(hostname -I)
 
-
-turnserver -o -f -a -n -L $privateip -p 3001 -X $publicip -E $privateip -u user:password  -r $publichostname
+turnserver -o -f -a -n -p 3001 -X $publicip -u user:password  -r $publichostname
 
